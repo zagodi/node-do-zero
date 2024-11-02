c6752ba21f3176b23cba626a2333d562230f2f45
Zagodi <diego.s.zago1995@gmail.com>
2024-11-02 15:01:53 -0300

    Adding host do server config

--- server.js
+++ server.js
@@ -54,4 +54,4 @@ server.delete("/videos/:id", async (request, reply) => {
   return reply.status(200).send();
 });
 
-server.listen({ port: process.env.PORT ?? 3333 });
+server.listen({ host: "0.0.0.0", port: process.env.PORT ?? 3333 });
