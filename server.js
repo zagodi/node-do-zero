ae4f31e3f103adad9f13d6ce6a6ce39e7260c11e
Zagodi <diego.s.zago1995@gmail.com>
2024-11-02 14:55:35 -0300

    Node api using postgress and fastify

---
+++ server.js
@@ -0,0 +1,57 @@
+// import { createServer } from "node:http";
+
+// const server = createServer((request, response) => {
+//   console.log("oi");
+// });
+
+// server.listen(3333);
+
+// import { DatabaseMemory } from "./database-memory.js";
+import { fastify } from "fastify";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+
+const database = new DatabasePostgres();
+
+server.post("/videos", async (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get("/videos", async (request, reply) => {
+  const search = request.query.search;
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+server.put("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+  await database.delete(videoId);
+
+  return reply.status(200).send();
+});
+
+server.listen({ port: process.env.PORT ?? 3333 });
